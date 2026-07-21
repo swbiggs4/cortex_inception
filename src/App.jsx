2de66b5f582b3fbb08e120bb9fce87b808b2f3c4
Sean Grullon <sgrullon@madrigalpharma.com>
2026-07-21 15:20:20 -0700

    launch homeroom landing page

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '100px' }}>
-      <h1>Hello, World!</h1>
-      <p>My first website is live.</p>
-    </div>
-  )
-}
