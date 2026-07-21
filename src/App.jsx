4e6f0697e6a86a9a23e7ee73d04ed08db81daced
Sean Grullon <sgrullon@madrigalpharma.com>
2026-07-21 14:52:57 -0700

    initial scaffold

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '100px' }}>
+      <h1>Hello, World!</h1>
+      <p>My first website is live.</p>
+    </div>
+  )
+}
